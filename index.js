62d633a8975215f443a4176a461fe0853dc47b2e
PedroNnsM <pedronunes95@outlook.com>
2022-08-14 22:28:17 -0300

    metodo post

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const app = express();
 
 app.listen('3000');
 
-app.route('/').get( (req, res) => {
-    res.send('Hello World');
-} );
 
-app.route('/sobre').get( (req, res) => {
-    res.send('Hello World sobre, vai que nao tem nada')
-} );
+// Get request
+
+// app.route('/').get( (req, res) => {
+//     res.send('Hello World');
+// } );
+
+// app.route('/sobre').get( (req, res) => {
+//     res.send('Hello World sobre, vai que nao tem nada')
+// } );
+
+// middleware 
+
+app.use( express.json() );
+
+app.route('/').post( (req, res) =>  res.send(req.body) );
